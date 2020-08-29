9fb5382de02c3bc9389a07baef51f1cb72fe542b
Xiaoyi Liu <lxy878@gmail.com>
2020-08-29 22:09:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+
+function map(array, cb){
+  let results = [];
+  for (const n of array)
+    results.push(cb(n));
+  return results;
+}
+
+function reduce(array, cb, p){
+  let result = p ? p + array[0] : array[0];
+  for (let i=1; i < array.length; i++)
+    result = cb(array[i], result);
+  return result;
+}
